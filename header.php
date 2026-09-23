39a45282fb4387cef259adcc0d68323f2a43c1d6
CasperCurio <D301710@edu.curio.nl>
2026-09-23 13:24:07 +0200

    styling index bijgewerkt

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <header>
-    <img src="img/logo.png" alt="foto logo">
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="aboutus.php">Over ons</a>
-        <a href="products.php">Producten</a>
-    </nav>
+    <div class="wrapper">
+        <img src="img/logo.png" alt="foto logo">
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="aboutus.php">Over ons</a>
+            <a href="products.php">Producten</a>
+        </nav>
+    </div>
 </header>
